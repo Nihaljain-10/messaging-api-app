8f444bc157f6b2147ca79bc759da758d53bb1736
Nihaljain-10 <nihalj10jain@gmail.com>
2024-07-12 14:29:33 +0530

    first commit

---
+++ src/test/java/com/training/bms/MessagingApiAppApplicationTests.java
@@ -0,0 +1,15 @@
+package com.training.bms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class MessagingApiAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertEquals(190,200);
+	}
+
+}
