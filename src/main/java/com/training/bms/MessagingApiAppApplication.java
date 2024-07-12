2fcb419f7600ca36a0cffabf75676a3e008bcf10
Nihaljain-10 <nihalj10jain@gmail.com>
2024-07-12 15:09:59 +0530

    added changes

--- src/main/java/com/training/bms/MessagingApiAppApplication.java
+++ src/main/java/com/training/bms/MessagingApiAppApplication.java
@@ -2,9 +2,11 @@ package com.training.bms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class MessagingApiAppApplication {
+	@GetMapping("weekend")
 	public void printGreetings() {
 		System.out.println("Hi Oracle");
 	}
